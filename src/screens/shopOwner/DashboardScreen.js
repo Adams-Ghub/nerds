d474152e6523dcf9e5e70e2ea4a4c9141b729e85
LovelaceFilson <58467784+LovelaceFilson@users.noreply.github.com>
2021-03-17 19:09:55 +0000

    Created Dashboard Screen

---
+++ src/screens/shopOwner/DashboardScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import DrawerItem from "../../components/DrawerItem";
+
+const DashboardScreen = () => {
+  return (
+    <View>
+      <DrawerItem />
+    </View>
+  );
+};
+
+export default DashboardScreen;
+
+const styles = StyleSheet.create({});

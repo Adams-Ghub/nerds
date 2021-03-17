8bba4fe0eccea4cdf001f5dea7b194a11a96b9f0
LovelaceFilson <58467784+LovelaceFilson@users.noreply.github.com>
2021-03-17 20:04:08 +0000

    Added Line Graph to Shop Owner Dashboard

--- src/screens/shopOwner/DashboardScreen.js
+++ src/screens/shopOwner/DashboardScreen.js
@@ -1,11 +1,51 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import DrawerItem from "../../components/DrawerItem";
-
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { LineChart } from "react-native-chart-kit";
+LineChart;
 const DashboardScreen = () => {
   return (
     <View>
-      <DrawerItem />
+      <View>
+        <LineChart
+          data={{
+            labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                ],
+              },
+            ],
+          }}
+          width={Dimensions.get("window").width} // from react-native
+          height={320}
+          yAxisLabel="¢"
+          yAxisSuffix="k"
+          yAxisInterval={10} // optional, defaults to 1
+          chartConfig={{
+            backgroundGradientFrom: "#ffffff",
+            backgroundGradientTo: "#ffffff",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+
+            propsForDots: {
+              r: "4",
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      </View>
     </View>
   );
 };

e35e3b73b596627feae95b28b1c218f9a34f1164
LovelaceFilson <58467784+LovelaceFilson@users.noreply.github.com>
2021-04-25 18:29:18 +0000

    Added styles to the stylesheet

--- src/screens/customer/CheckoutSummaryScreen.js
+++ src/screens/customer/CheckoutSummaryScreen.js
@@ -145,7 +145,7 @@ const styles = StyleSheet.create({
     elevation: 1,
   },
   buttonContainer: {
-    backgroundColor: "#0080FF",
+    backgroundColor: "#080809",
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 6,

4c01ea5d10011b2f3000a31e134d933b87db3938
LovelaceFilson <58467784+LovelaceFilson@users.noreply.github.com>
2021-04-25 18:30:28 +0000

    Updated Success screen

--- src/screens/customerMomoPayment/SuccessScreen.js
+++ src/screens/customerMomoPayment/SuccessScreen.js
@@ -1,11 +1,40 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 
-const SuccessScreen = () => {
+const SuccessScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.payment}> Payment Successful</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Image
+          source={require("../../../assets/payment_successful.jpg")}
+          style={styles.image}
+        />
+        <Text style={styles.payment}>Payment Successful</Text>
+        <View style={styles.button}>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.proceed}>Track Order Status</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.button}>
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={() => {
+              navigation.navigate("CustomerDashboard");
+            }}
+          >
+            <Text style={styles.proceed}>Back to home</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -16,6 +45,33 @@ const styles = StyleSheet.create({
     alignSelf: "center",
   },
   payment: {
+    marginTop: 5,
     fontWeight: "bold",
+    fontSize: 30,
+  },
+
+  image: {
+    marginTop: 20,
+    height: 300,
+    width: 300,
+    borderRadius: 150,
+  },
+  buttonContainer: {
+    backgroundColor: "#080809",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 6,
+    height: 40,
+    width: 300,
+    marginVertical: 40,
+  },
+
+  proceed: {
+    color: "#ffffff",
+    fontSize: 16,
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
